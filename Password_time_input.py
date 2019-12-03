228450e0a6673d5ab08ecac2cf130f84cc34014b
Anthony-T-N <53560209+Anthony-T-N@users.noreply.github.com>
2019-12-03 22:20:32 +1030

    Update Password_time_input.py

--- Password_time_input.py
+++ Password_time_input.py
@@ -2,4 +2,7 @@ import time;
 
 localTime = time.localtime(time.time());
 localTime_str = str(localTime);
-localTime_str = localTime_str.strip('time.struct_time()');
+localTime_str = localTime_str.strip("time.struct_time()");
+# Now a list
+localTime_str = localTime_str.split(" ");
+print(localTime_str);
