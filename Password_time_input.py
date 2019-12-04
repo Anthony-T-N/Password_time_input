d4c8b7963ba1d5b9dabe58fc39fa51e83024d11e
Anthony-T-N <53560209+Anthony-T-N@users.noreply.github.com>
2019-12-04 22:23:35 +1030

    Update Password_time_input.py

--- Password_time_input.py
+++ Password_time_input.py
@@ -6,3 +6,17 @@ localTime_str = localTime_str.strip("time.struct_time()");
 # Now a list
 localTime_str = localTime_str.split(" ");
 print(localTime_str);
+
+x = 0;
+z = 0;
+variable_1 = 0;
+variable_2 = 0;
+for i in localTime_str:
+  i = i.split('=', 1)[-1];
+  i = i[:-1];
+  print(i);
+  if (x == 2):
+    variable_1 = i;
+  if (x == 3):
+    variable_2 = i;
+  x = x + 1;
