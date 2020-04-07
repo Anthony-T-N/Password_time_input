da4b1982c854f37ddc0fc2d6210f461830221f6a
Anthony-T-N <53560209+Anthony-T-N@users.noreply.github.com>
2020-04-07 15:28:12 +0930

    Update Password_time_input.py

--- Password_time_input.py
+++ Password_time_input.py
@@ -25,7 +25,7 @@ def local_time_extraction():
 
     """
     localTime = time.localtime(time.time())
-    localTime_str = str(localTime);
+    localTime_str = str(localTime)
     localTime_str = localTime_str.strip("time.struct_time()")
     # Now a list
     localTime_str = localTime_str.split(" ")
